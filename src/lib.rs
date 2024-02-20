793a79331fb069c5ebd16e211b243eb93cc6050d
Tim (Theemathas) Chirananthavat <theemathas@gmail.com>
2024-02-20 19:01:24 +0700

    Separate out producers into a new module.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 //! See the documentation for [`InfVec`] for usage information.
 
+pub mod producer;
+pub use producer::Producer;
+
+use producer::{FnMutProducer, IteratorProducer};
 use std::{
     array,
     cell::RefCell,
@@ -15,48 +19,7 @@ const CHUNK_SIZE: usize = 64;
 
 // TODO Cargo.toml
 
-// TODO figure out clone
-
-/// A trait for types that can produce an infinite stream of elements of type
-/// `T`.
-pub trait Producer<T> {
-    fn produce(&mut self) -> T;
-}
-
-impl<T, P: Producer<T> + ?Sized> Producer<T> for &mut P {
-    fn produce(&mut self) -> T {
-        (**self).produce()
-    }
-}
-
-impl<T, P: Producer<T> + ?Sized> Producer<T> for Box<P> {
-    fn produce(&mut self) -> T {
-        (**self).produce()
-    }
-}
-
-/// A [`Producer`] that produces elements by repeatedly calling a closure.
-#[derive(Debug)]
-pub struct FnMutProducer<F>(pub F);
-
-impl<T, F: FnMut() -> T> Producer<T> for FnMutProducer<F> {
-    fn produce(&mut self) -> T {
-        self.0()
-    }
-}
-
-/// A [`Producer`] that produces elements from an iterator which is assumed to
-/// be infinite. Panics if the iterator runs out of elements.
-#[derive(Debug)]
-pub struct IteratorProducer<I>(pub I);
-
-impl<I: Iterator> Producer<I::Item> for IteratorProducer<I> {
-    fn produce(&mut self) -> I::Item {
-        self.0
-            .next()
-            .expect("An IteratorProducer should never run out of elements")
-    }
-}
+// TODO figure out clone (for InfVec and for producers)
 
 // TODO more docs
 
