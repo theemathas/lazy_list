6cdb56c19e5e81d20beee4b1310410f242b43b11
Tim (Theemathas) Chirananthavat <theemathas@gmail.com>
2024-02-22 18:59:31 +0700

    Use iterators instead of producers.

--- src/infinite_vec.rs
+++ src/infinite_vec.rs
@@ -10,41 +10,41 @@ use std::{
     ptr::NonNull,
 };
 
-use crate::producer::{FnMutProducer, IteratorProducer, Producer};
-
 /// Number of elements in a chunk in the cache
 const CHUNK_SIZE: usize = 64;
 
-// TODO figure out clone (for InfVec and for producers)
+// TODO figure out clone
 // TODO more docs
 // TODO map, filter, etc.
 // TODO Debug impl for iterator
 
-/// A lazily-populated infinite `Vec`-like data structure, the main type of
-/// this crate.
+/// A lazily-populated infinite `Vec`-like data structure, the main type of this
+/// crate.
 ///
-/// An `InfVec<T, P>` contains infinitely many values of type `T`.  It can be
+/// An `InfVec<T, I>` contains infinitely many values of type `T`.  It can be
 /// iterated over and indexed into similarly to a `Vec`. Elements are produced
-/// on-demand by a [`Producer`] with type `P` which is specified upon creation
-/// of the `InfVec`. The elements can be later accessed by index, and can be
+/// on-demand by a iterator with type `I` which is specified upon creation of
+/// the `InfVec`. The elements can be later accessed by index, and can be
 /// modified.
 ///
+/// If the iterator is not infinite, operations on the `InfVec` might panic.
+///
 /// `InfVec` is currently not thread-safe. It is also invariant, as opposed to
-/// covariant.
+/// covariant, if that matters to you.
 ///
 /// Despite the name, `InfVec` doesn't actually store elements continuously like
 /// a `Vec`. Instead, it stores elements in 64-element chunks. This is so that
 /// we can hand out references to elements, and not have them be invalidated as
 /// more elements are added to the cache.
 ///
-/// If you don't want to specify a producer type as a type parameter, you can
+/// If you don't want to specify an iterator type as a type parameter, you can
 /// use the [`InfVecBoxed`] type alias.
-pub struct InfVec<T, P>(RefCell<InfVecInner<T, P>>, PhantomData<(Vec<T>, P)>);
+pub struct InfVec<T, I>(RefCell<InfVecInner<T, I>>, PhantomData<(Vec<T>, I)>);
 
-/// A type alias for an [`InfVec`] with a boxed [`Producer`] for convenience.
+/// A type alias for an [`InfVec`] with a boxed iterator for convenience.
 ///
 /// In most cases, `InfVecBoxed<'static, T>` is the type you want.
-pub type InfVecBoxed<'a, T> = InfVec<T, Box<dyn Producer<T> + 'a>>;
+pub type InfVecBoxed<'a, T> = InfVec<T, Box<dyn Iterator<Item = T> + 'a>>;
 
 type Chunk<T> = NonNull<[MaybeUninit<T>; CHUNK_SIZE]>;
 
@@ -55,7 +55,7 @@ type Chunk<T> = NonNull<[MaybeUninit<T>; CHUNK_SIZE]>;
 ///
 /// Non-safety invariant when panics do not occur: After the first `cached_len`
 /// elements, the remaining elements are uninitialized.
-struct InfVecInner<T, P> {
+struct InfVecInner<T, I> {
     /// The elements that have been produced so far and cached. The i'th cached
     /// element is at `cached_chunks[i / CHUNK_SIZE][i % CHUNK_SIZE]`.
     ///
@@ -66,16 +66,15 @@ struct InfVecInner<T, P> {
     cached_chunks: Vec<Chunk<T>>,
     /// The number of elements that have been produced and cached so far.
     num_cached: usize,
-    /// The producer of elements, which will be called to acquire elements that
-    /// haven't been cached yet.
-    producer: P,
+    /// The iterator that will produce the elements that haven't been cached yet.
+    remaining: I,
 }
 
 /// Iterator over immutable references to the elements of an [`InfVec`].
 ///
 /// This struct is created by the [`iter`](InfVec::iter) method on [`InfVec`].
-pub struct Iter<'a, T, P> {
-    vec: &'a InfVec<T, P>,
+pub struct Iter<'a, T, I> {
+    vec: &'a InfVec<T, I>,
     index: usize,
 }
 
@@ -83,26 +82,26 @@ pub struct Iter<'a, T, P> {
 ///
 /// This struct is created by the [`iter_mut`](InfVec::iter_mut) method on
 /// [`InfVec`].
-pub struct IterMut<'a, T, P> {
-    vec: &'a mut InfVec<T, P>,
+pub struct IterMut<'a, T, I> {
+    vec: &'a mut InfVec<T, I>,
     index: usize,
 }
 
 /// Iterator that moves elements out of an [`InfVec`].
 ///
 /// This struct is created by the `into_iter` method on [`InfVec`].
-pub struct IntoIter<T, P> {
+pub struct IntoIter<T, I> {
     // SAFETY invariant: Instead of `InfVec` having elements `0..num_cached`
     // initialized, this one has elements `index..num_cached` initialized.
-    vec: InfVec<T, P>,
+    vec: InfVec<T, I>,
     index: usize,
 }
 
 // SAFETY: We don't rely on everything being in one thread. We don't have `Sync`
 // though.
-unsafe impl<T: Send, P: Send> Send for InfVec<T, P> {}
+unsafe impl<T: Send, I: Send> Send for InfVec<T, I> {}
 
-impl<T, P> Drop for InfVec<T, P> {
+impl<T, I> Drop for InfVec<T, I> {
     fn drop(&mut self) {
         let mut inner = self.0.borrow_mut();
         // SAFETY: Only drops initialized elements as per the invariant of
@@ -123,7 +122,7 @@ impl<T, P> Drop for InfVec<T, P> {
     }
 }
 
-impl<T, P> Drop for IntoIter<T, P> {
+impl<T, I> Drop for IntoIter<T, I> {
     fn drop(&mut self) {
         let mut inner = self.vec.0.borrow_mut();
 
@@ -150,7 +149,7 @@ impl<T, P> Drop for IntoIter<T, P> {
     }
 }
 
-impl<T, P> InfVecInner<T, P> {
+impl<T, I> InfVecInner<T, I> {
     fn index_ptr(&self, i: usize) -> *const MaybeUninit<T> {
         let chunk = self.cached_chunks[i / CHUNK_SIZE];
         // We do this janky way to avoid creating a reference to the chunk,
@@ -170,16 +169,17 @@ impl<T, P> InfVecInner<T, P> {
     }
 }
 
-impl<T, P> InfVec<T, P> {
-    /// Creates a new `InfVec` with the given [`Producer`]. The resulting
-    /// `InfVec` is conceptually initialized by values from successive calls to
-    /// [`produce`](Producer::produce).
-    pub const fn new(producer: P) -> InfVec<T, P> {
+impl<T, I> InfVec<T, I> {
+    /// Creates an [`InfVec`] from an iterator. The resulting `InfVec`
+    /// conceptually contains the stream of elements produced by the iterator.
+    /// Operations on the resulting `InfVec` might panic if the iterator is not
+    /// infinite.
+    pub const fn new(iterator: I) -> InfVec<T, I> {
         InfVec(
             RefCell::new(InfVecInner {
                 cached_chunks: Vec::new(),
                 num_cached: 0,
-                producer,
+                remaining: iterator,
             }),
             PhantomData,
         )
@@ -191,41 +191,17 @@ impl<T, P> InfVec<T, P> {
     }
 }
 
-impl<T, F: FnMut() -> T> InfVec<T, FnMutProducer<F>> {
-    /// Creates a new `InfVec` from a closure. The resulting `InfVec` is
-    /// conceptually initialized by values from successive calls to the closure.
-    pub fn from_fn(f: F) -> InfVec<T, FnMutProducer<F>> {
-        InfVec::new(FnMutProducer(f))
-    }
-}
-
-impl<I: Iterator> InfVec<I::Item, IteratorProducer<I>> {
-    /// Creates a new `InfVec` from an iterator, conceptually containing the
-    /// stream of elements produced by the iterator. Operations on the resulting
-    /// `InfVec` might panic if the iterator is not infinite.
-    pub fn from_infinite_iter(iter: I) -> InfVec<I::Item, IteratorProducer<I>> {
-        InfVec::new(IteratorProducer(iter))
-    }
-}
-
 impl<'a, T> InfVecBoxed<'a, T> {
-    /// Creates an [`InfVecBoxed`] from a closure. This method boxes the
-    /// closure for you. The resulting `InfVecBoxed` is conceptually initialized
-    /// by values from successive calls to the closure.
-    pub fn boxed_from_fn(f: impl FnMut() -> T + 'a) -> InfVecBoxed<'a, T> {
-        InfVecBoxed::new(Box::new(FnMutProducer(f)))
-    }
-
     /// Creates an [`InfVecBoxed`] from an iterator. This method boxes the
     /// iterator for you. The resulting `InfVec` conceptually contains the
     /// stream of elements produced by the iterator. Operations on the resulting
     /// `InfVecBoxed` might panic if the iterator is not infinite.
     pub fn boxed_from_infinite_iter(iter: impl Iterator<Item = T> + 'a) -> InfVecBoxed<'a, T> {
-        InfVecBoxed::new(Box::new(IteratorProducer(iter)))
+        InfVecBoxed::new(Box::new(iter))
     }
 }
 
-impl<T, P: Producer<T>> Index<usize> for InfVec<T, P> {
+impl<T, I: Iterator<Item = T>> Index<usize> for InfVec<T, I> {
     type Output = T;
 
     fn index(&self, index: usize) -> &T {
@@ -239,7 +215,7 @@ impl<T, P: Producer<T>> Index<usize> for InfVec<T, P> {
     }
 }
 
-impl<T, P: Producer<T>> IndexMut<usize> for InfVec<T, P> {
+impl<T, I: Iterator<Item = T>> IndexMut<usize> for InfVec<T, I> {
     fn index_mut(&mut self, index: usize) -> &mut T {
         self.ensure_cached(index);
         let mut guard = self.0.borrow_mut();
@@ -252,7 +228,7 @@ impl<T, P: Producer<T>> IndexMut<usize> for InfVec<T, P> {
     }
 }
 
-impl<T, P: Producer<T>> InfVec<T, P> {
+impl<T, I: Iterator<Item = T>> InfVec<T, I> {
     /// Ensures that element `i` is cached.
     fn ensure_cached(&self, i: usize) {
         let mut guard = self.0.borrow_mut();
@@ -276,14 +252,19 @@ impl<T, P: Producer<T>> InfVec<T, P> {
             let target_element_ref: &mut MaybeUninit<T> = unsafe { &mut *target_element_ptr };
             // A panic here won't corrupt anything since we haven't updated
             // cached_len yet.
-            target_element_ref.write(inner.producer.produce());
+            target_element_ref.write(
+                inner
+                    .remaining
+                    .next()
+                    .expect("The iterator used to construct an InfVec should be infinite"),
+            );
 
             inner.num_cached += 1;
         }
     }
 
     /// Returns an infinite iterator over the elements of the `InfVec`.
-    pub fn iter(&self) -> Iter<T, P> {
+    pub fn iter(&self) -> Iter<T, I> {
         Iter {
             vec: self,
             index: 0,
@@ -291,7 +272,7 @@ impl<T, P: Producer<T>> InfVec<T, P> {
     }
 
     /// Returns a mutable infinite iterator over the elements of the `InfVec`.
-    pub fn iter_mut(&mut self) -> IterMut<T, P> {
+    pub fn iter_mut(&mut self) -> IterMut<T, I> {
         IterMut {
             vec: self,
             index: 0,
@@ -299,11 +280,11 @@ impl<T, P: Producer<T>> InfVec<T, P> {
     }
 }
 
-impl<T, P: Producer<T>> IntoIterator for InfVec<T, P> {
+impl<T, I: Iterator<Item = T>> IntoIterator for InfVec<T, I> {
     type Item = T;
-    type IntoIter = IntoIter<T, P>;
+    type IntoIter = IntoIter<T, I>;
 
-    fn into_iter(self) -> IntoIter<T, P> {
+    fn into_iter(self) -> IntoIter<T, I> {
         IntoIter {
             vec: self,
             index: 0,
@@ -311,25 +292,25 @@ impl<T, P: Producer<T>> IntoIterator for InfVec<T, P> {
     }
 }
 
-impl<'a, T, P: Producer<T>> IntoIterator for &'a InfVec<T, P> {
+impl<'a, T, I: Iterator<Item = T>> IntoIterator for &'a InfVec<T, I> {
     type Item = &'a T;
-    type IntoIter = Iter<'a, T, P>;
+    type IntoIter = Iter<'a, T, I>;
 
-    fn into_iter(self) -> Iter<'a, T, P> {
+    fn into_iter(self) -> Iter<'a, T, I> {
         self.iter()
     }
 }
 
-impl<'a, T, P: Producer<T>> IntoIterator for &'a mut InfVec<T, P> {
+impl<'a, T, I: Iterator<Item = T>> IntoIterator for &'a mut InfVec<T, I> {
     type Item = &'a mut T;
-    type IntoIter = IterMut<'a, T, P>;
+    type IntoIter = IterMut<'a, T, I>;
 
-    fn into_iter(self) -> IterMut<'a, T, P> {
+    fn into_iter(self) -> IterMut<'a, T, I> {
         self.iter_mut()
     }
 }
 
-impl<'a, T, P: Producer<T>> Iterator for Iter<'a, T, P> {
+impl<'a, T, I: Iterator<Item = T>> Iterator for Iter<'a, T, I> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<&'a T> {
@@ -339,7 +320,7 @@ impl<'a, T, P: Producer<T>> Iterator for Iter<'a, T, P> {
     }
 }
 
-impl<'a, T, P: Producer<T>> Iterator for IterMut<'a, T, P> {
+impl<'a, T, I: Iterator<Item = T>> Iterator for IterMut<'a, T, I> {
     type Item = &'a mut T;
 
     fn next<'b>(&'b mut self) -> Option<&'a mut T> {
@@ -351,7 +332,7 @@ impl<'a, T, P: Producer<T>> Iterator for IterMut<'a, T, P> {
     }
 }
 
-impl<T, P: Producer<T>> Iterator for IntoIter<T, P> {
+impl<T, I: Iterator<Item = T>> Iterator for IntoIter<T, I> {
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
@@ -365,12 +346,17 @@ impl<T, P: Producer<T>> Iterator for IntoIter<T, P> {
             self.index += 1;
             Some(result)
         } else {
-            Some(inner.producer.produce())
+            Some(
+                inner
+                    .remaining
+                    .next()
+                    .expect("The iterator used to construct an InfVec should be infinite"),
+            )
         }
     }
 }
 
-impl<T: Debug, P> Debug for InfVec<T, P> {
+impl<T: Debug, I> Debug for InfVec<T, I> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // Hack until DebugList::entries is stabilized
         struct DebugEllipsis;
@@ -396,15 +382,15 @@ impl<T: Debug, P> Debug for InfVec<T, P> {
 mod tests {
     use super::*;
 
-    use std::sync::atomic::AtomicUsize;
+    use std::{iter, sync::atomic::AtomicUsize};
 
     #[test]
     fn test_indexing() {
         let mut x = 0;
-        let mut vec = InfVec::from_fn(move || {
+        let mut vec = InfVec::new(iter::repeat_with(move || {
             x += 1;
             x
-        });
+        }));
         for i in 0..100 {
             assert_eq!(vec[i], i + 1);
         }
@@ -420,18 +406,6 @@ mod tests {
     }
 
     #[test]
-    fn test_boxed_from_fn() {
-        let mut x = 0;
-        let vec: InfVecBoxed<'_, _> = InfVecBoxed::boxed_from_fn(move || {
-            x += 1;
-            x
-        });
-        for i in 0..100 {
-            assert_eq!(vec[i], i + 1);
-        }
-    }
-
-    #[test]
     fn test_boxed_from_infinite_iter() {
         let vec: InfVecBoxed<'_, _> = InfVecBoxed::boxed_from_infinite_iter((0..).map(|x| x * x));
         for i in 0..100 {
@@ -442,10 +416,10 @@ mod tests {
     #[test]
     fn test_iter() {
         let mut x = 0;
-        let vec = InfVec::from_fn(move || {
+        let vec = InfVec::new(iter::repeat_with(move || {
             x += 1;
             x
-        });
+        }));
         let mut iter = vec.iter();
         for i in 0..100 {
             assert_eq!(iter.next(), Some(&(i + 1)));
@@ -462,10 +436,10 @@ mod tests {
     #[test]
     fn test_iter_mut() {
         let mut x = 0;
-        let mut vec = InfVec::from_fn(move || {
+        let mut vec = InfVec::new(iter::repeat_with(move || {
             x += 1;
             x
-        });
+        }));
         let mut iter = vec.iter_mut();
         for i in 0..100 {
             let element_ref = iter.next().unwrap();
@@ -493,10 +467,10 @@ mod tests {
         let drop_counter = AtomicUsize::new(0);
         let drop_counter_ref = &drop_counter;
         let mut x = 0;
-        let vec = InfVec::from_fn(move || {
+        let vec = InfVec::new(iter::repeat_with(move || {
             x += 1;
             DropCounter(drop_counter_ref, x)
-        });
+        }));
 
         let mut iter = vec.iter();
         for _ in 0..200 {
@@ -518,10 +492,10 @@ mod tests {
     #[test]
     fn test_debug() {
         let mut x = 0;
-        let vec = InfVec::from_fn(move || {
+        let vec = InfVec::new(iter::repeat_with(move || {
             x += 1;
             x
-        });
+        }));
         assert_eq!(format!("{:?}", vec), "[...]");
         vec[9];
         assert_eq!(format!("{:?}", vec), "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...]");
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 //! See the documentation for [`InfVec`] for usage information.
 
 pub mod infinite_vec;
-pub mod producer;
 
 pub use infinite_vec::InfVec;
-pub use producer::Producer;
