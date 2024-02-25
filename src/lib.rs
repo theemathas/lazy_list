779751ce9d0566ae7e67455be8ce2fe618bfa485
Tim (Theemathas) Chirananthavat <theemathas@gmail.com>
2024-02-25 12:58:10 +0700

    Add LazyList type

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
 #![warn(missing_docs)]
 #![allow(clippy::missing_panics_doc, clippy::module_name_repetitions)]
-//! This crate provides lazily-populated lists, infinite or infinite.
+//! This crate provides lazily-populated lists, finite or infinite.
 //!
-//! For usage examples, see the documentation for the [`inf_list`] module.
+//! For usage examples, see the documentation for the [`lazy_list`] and
+//! [`inf_list`] modules.
 
 mod chunked_vec;
 pub mod inf_list;
+pub mod lazy_list;
 
 pub use inf_list::{InfList, InfListBoxed, InfListOwned};
+pub use lazy_list::{LazyList, LazyListBoxed, LazyListOwned};
 
 /// Extension trait for [`Iterator`], providing lazy list operations.
 pub trait IteratorLazyExt: Iterator + Sized {
@@ -16,9 +19,16 @@ pub trait IteratorLazyExt: Iterator + Sized {
     ///
     /// Equivalent to [`InfList::new`].
     fn collect_inf<T>(self) -> InfList<T, Self>;
+    /// Collects the elements of an iterator into a [`LazyList`].
+    ///
+    /// Equivalent to [`LazyList::new`].
+    fn collect_lazy<T>(self) -> LazyList<T, Self>;
 }
 impl<I: Iterator + Sized> IteratorLazyExt for I {
     fn collect_inf<T>(self) -> InfList<T, Self> {
         InfList::new(self)
+    }
+    fn collect_lazy<T>(self) -> LazyList<T, Self> {
+        LazyList::new(self)
     }
 }
